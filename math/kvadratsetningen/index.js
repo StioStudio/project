8af4b18b828c0c6f9cae7b213417a552f19b205a
StioStudio <simon.s.johanssen@gmail.com>
2023-09-21 16:36:48 +0200

    kvadratsetningen

---
+++ math/kvadratsetningen/index.js
@@ -0,0 +1,34 @@
+document.querySelector(".input-1").value = 1
+document.querySelector(".input-2").value = 6
+document.querySelector(".input-3").value = 9
+
+let input = {
+    a: document.querySelector(".input-1").value,
+    b: document.querySelector(".input-2").value,
+    c: document.querySelector(".input-3").value
+}
+
+code()
+
+addEventListener("keypress", ()=>{
+    setTimeout(code, 10)
+})
+
+function code(){
+    input = {
+        a: document.querySelector(".input-1").value,
+        b: document.querySelector(".input-2").value,
+        c: document.querySelector(".input-3").value
+    }
+    let rem = (sumOgProdukt(input.a - 0, input.b - 0, input.c - 0))
+    console.log(rem)
+    document.querySelector(".output-1").innerHTML = rem[0]
+    document.querySelector(".output-2").innerHTML = rem[1]
+}
+
+function sumOgProdukt(a, b, c) {
+    return [
+        (((-b) - Math.sqrt((b**2) - (4*a*c))) / (2*a)),
+        (((-b) + Math.sqrt((b**2) - (4*a*c))) / (2*a))
+    ]
+}
