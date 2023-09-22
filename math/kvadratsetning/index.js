d0bd9e29d24364a172b4e331fbf3eef619e2790d
StioStudio <simon.s.johanssen@gmail.com>
2023-09-22 10:12:40 +0200

    fix

--- math/kvadratsetning/index.js
+++ math/kvadratsetning/index.js
@@ -28,7 +28,7 @@ function code(){
 
 function sumOgProdukt(a, b, c) {
     return [
-        (((-b) - Math.sqrt((b**2) - (4*a*c))) / (2*a))*(-1),
-        (((-b) + Math.sqrt((b**2) - (4*a*c))) / (2*a))*(-1)
+        (((-b) + Math.sqrt((b**2) - (4*a*c))) / (2*a)),
+        (((-b) - Math.sqrt((b**2) - (4*a*c))) / (2*a))
     ]
 }
