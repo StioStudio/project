648bcad40f0ad6820e7edc3d5ec5d31d83d29e85
stio <dev@stio.studio>
2023-11-09 15:56:45 +0100

    fix

---
+++ quote/index.js
@@ -0,0 +1,23 @@
+let options = {
+    "language": "en",
+    "themes": "dark"
+}
+let option = document.querySelectorAll(".options")
+
+for (let i = 0; i < Object.keys(options).length; i++) {
+    let rem = cookie.get(Object.keys(options)[i])
+    if(rem != "") {
+        options[Object.keys(options)[i]] = rem
+    }
+}
+
+option.forEach(e => {
+    // console.log(e.attributes.getNamedItem("name"))
+    e.value = options[e.attributes.getNamedItem("name").value]
+    e.addEventListener("change", ()=>{
+        cookie.set(e.attributes.getNamedItem("name").value, e.value)
+        console.log(cookie.raw())
+    })
+});
+
+await translationSetup()
