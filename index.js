459f85756f08a3c66106a870fc80dfa8287cfa53
stio <dev@stio.studio>
2024-01-05 22:19:51 +0100

    info.language

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
+info.language = info.language.cookie
 basicSetup()
 await lastUpdated({path: "index.html", repo: "project"})
 // let rem = await(await fetch("/assets/projects.json")).json()
 let rem = await(await fetch("./projects.json")).json()
 contentCreate(rem)
-
-await translationSetup(true)
