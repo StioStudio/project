2735ed32f3dbe2ba8e8b6490b4eebf3d5e51931f
stio <dev@stio.studio>
2024-01-14 00:13:17 +0100

    update

--- about-you/index.js
+++ about-you/index.js
@@ -3,32 +3,39 @@ await lastUpdated({path: "about-you/index.html", repo: "project"})
 
 const display = document.querySelector(".display")
 
-let meta = {}
+let outputs = {}
 document.querySelectorAll("output").forEach(e => {
-    meta[e.getAttribute("name")] = e
+    outputs[e.getAttribute("name")] = e
 })
 function update(_name, _value) {
-    meta[_name].elm.innerText = _value
+    outputs[_name].elm.innerText = _value
 }
-async function addOutput(_name, _value) {
-    display.insertAdjacentHTML("beforeend", `
+async function addOutput(_name, _value, {isLink = false} = {}) {
+    const output = isLink ? `
+    <div class="margin-8px">
+        <a name="${_name.remove(" ", "/")}" href="${_value}">${_name}</a>
+        <br>
+    </div>`
+    : `
     <div class="margin-8px">
         ${_name}: <output name="${_name.remove(" ", "/")}" class="output" type="text">...</output>
         <br>
-    </div>`)
-    const rem = display.querySelector(`.output[name="${_name.remove(" ", "/")}"]`)
-    meta[_name] = {
+    </div>`
+
+    display.insertAdjacentHTML("beforeend", output)
+    const rem = display.querySelector(`[name="${_name.remove(" ", "/")}"]`)
+    outputs[_name] = {
         name: _name,
         elm: rem,
         value: "..."
     }
     if(typeof _value == "function") {
-        _value(meta[_name].elm)
+        _value(outputs[_name].elm)
     }
     else {
         setTimeout(async ()=>{
-            meta[_name].value = await _value
-            meta[_name].elm.innerText = await _value
+            outputs[_name].value = await _value
+            if(!isLink)outputs[_name].elm.innerText = await _value
         })
     }
     return rem
@@ -40,8 +47,6 @@ addOutput("Inner Width", `${innerWidth}px`)
 addOutput("Outer Height", `${outerHeight}px`)
 addOutput("Outer Width", `${outerWidth}px`)
 addOutput("URL/link", location)
-addOutput("App Version", navigator.appVersion)
-addOutput("User Agent", navigator.userAgent)
 addOutput("Languages", navigator.languages)
 addOutput("Cookie Enabled?", navigator.cookieEnabled)
 addOutput("Logical processors available", navigator.hardwareConcurrency)
@@ -61,4 +66,26 @@ addOutput("continent", (await iss).continent)
 addOutput("country", (await iss).country)
 addOutput("latitude", (await iss).latitude)
 addOutput("longitude", (await iss).longitude)
+addOutput("Google maps link", `https://www.google.com/maps/place/${(await iss).latitude}+${(await iss).longitude}`, {isLink: true})
 addOutput("timezone", (await iss).timezone)
+
+addOutput("App Version", navigator.appVersion)
+addOutput("User Agent", navigator.userAgent)
+const _UAParser = new UAParser()
+addOutput("Browser", _UAParser.getBrowser().name)
+addOutput("Browser version", _UAParser.getBrowser().version)
+addOutput("CPU", _UAParser.getCPU().architecture)
+addOutput("Device model", _UAParser.getDevice().model)
+addOutput("Device type", _UAParser.getDevice().type)
+addOutput("Device vendor", _UAParser.getDevice().vendor)
+addOutput("Engine", _UAParser.getEngine().name)
+addOutput("Engine version", _UAParser.getEngine().version)
+addOutput("OS", _UAParser.getOS().name)
+addOutput("OS version", _UAParser.getOS().version)
+
+
+
+
+
+
+
